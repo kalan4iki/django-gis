a6c61da517b84566dfbd3bda9ced7ccbe5182a8c
kalan4iki <kalan-4@ya.ru>
2019-12-20 14:44:28 +0300

    Fix

--- gis/gis/urls.py
+++ gis/gis/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import admin_tools
 
 urlpatterns = [
     path('admin/', include('admin_tools.urls')),
