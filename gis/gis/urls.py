0d282656845594d50bad81c8f5e4e128b2e6f780
kalan4iki <kalan-4@ya.ru>
2019-12-20 14:54:39 +0300

    fix

--- gis/gis/urls.py
+++ gis/gis/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', include('admin_tools.urls')),
+    url(r'^admin/', include('admin_tools.urls')),
     #path('admin/', admin.site.urls),
     path('', include('bot.urls')),
 ]
